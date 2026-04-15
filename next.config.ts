cdd7424a5ffa86bcd950f8ec87501f23c3c969ad
mohamed <mhomedabdo2271@gmail.com>
2026-04-15 02:20:07 +0200

    save

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  images: { unoptimized: true },
+  allowedDevOrigins: ["192.168.1.6"],
 };
+
 
 export default nextConfig;
